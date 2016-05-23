bac26bbe89cf404516077c4a80f8fc2fceb59dcf
grid <gridsane@gmail.com>
2016-05-23 11:39:55 +0500

    header-cover scss

--- src/components/header/header-cover.js
+++ src/components/header/header-cover.js
@@ -1,65 +1,22 @@
 import React, {Component, PropTypes} from 'react';
-import Radium from 'radium';
-import {colors} from '../../utils/styles';
+import styles from './header.scss';
 
-@Radium
 export default class HeaderCover extends Component {
   static propTypes = {
     url: PropTypes.string,
-  };
+  }
 
   static defaultProps = {
     url: null,
-  };
+  }
 
   render() {
-    const styles = this.getStyles();
-
-    return <div style={styles.container}>
-        <div style={styles.cover} />
-    </div>;
-  }
-
-  getStyles() {
     const {url} = this.props;
 
-    return {
-      container: {
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        overflow: 'hidden',
-        zIndex: -1,
-      },
-
-      cover: {
-        position: 'absolute',
-        top: -12,
-        right: -12,
-        bottom: -12,
-        left: -12,
-        backgroundColor: colors.primary,
-        backgroundImage: url ? `url(${url})` : 'none',
-        backgroundPositionX: 'center',
-        backgroundRepeatY: 'repeat',
-        backgroundSize: 'cover',
-        WebkitFilter: 'blur(8px)',
-        animation: 'x 60s linear infinite alternate',
-        animationName: coverSliding,
-        opacity: .5,
-        transition: 'background 2s ease-out',
-      },
-    };
+    return <div className={styles.cover}>
+        <div className={styles.coverImage} style={{
+          backgroundImage: url ? `url(${url})` : 'none',
+        }} />
+    </div>;
   }
 }
-
-const coverSliding = Radium.keyframes({
-  'from': {
-    backgroundPositionY: '0%',
-  },
-  'to': {
-    backgroundPositionY: '100%',
-  },
-});

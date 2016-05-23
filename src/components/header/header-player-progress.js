72e087516cd008d656e2a0f5716e1aed328dc356
grid <gridsane@gmail.com>
2016-05-23 12:05:21 +0500

    header-player-progress scss

--- src/components/header/header-player-progress.js
+++ src/components/header/header-player-progress.js
@@ -1,9 +1,8 @@
 import React, {Component, PropTypes} from 'react';
-import Radium from 'radium';
-import {colors} from '../../utils/styles';
-import {nodeOffset, curried} from '../../utils/common';
+import {nodeOffset} from '../../utils/common';
+import cn from 'classnames';
+import styles from './header.scss';
 
-@Radium
 export default class HeaderPlayerProgress extends Component {
 
   static propTypes = {
@@ -12,41 +11,37 @@ export default class HeaderPlayerProgress extends Component {
     onSeek: PropTypes.func.isRequired,
     isLoading: PropTypes.bool,
     canSeek: PropTypes.bool,
-  };
+  }
 
   static defaultProps = {
     canSeek: true,
-  };
-
-  state = {
-    mouseEnter: false,
-  };
+  }
 
   render() {
-    const styles = this.getStyles();
+    const {isLoading, canSeek} = this.props;
+    const progressPercent = ((100 / this.props.max) * this.props.current) + '%';
 
     return <div ref="progress"
-      style={styles.container}
-      onMouseEnter={curried(::this._mouseEnter, true)}
-      onMouseLeave={curried(::this._mouseEnter, false)}
-      onClick={::this._seek}>
-      <div style={styles.background} />
-      <div style={styles.progress} />
-      <div style={styles.pointer} ref="pointer">
-        <div style={styles.pointerGlow} />
-      </div>
+      className={cn(styles.progress, {
+        [styles.progressSeekable]: canSeek,
+      })}
+      onClick={this._seek}>
+
+      <div className={cn(styles.progressBackground, {
+        [styles.progressBackgroundLoading]: isLoading,
+      })} />
+
+      <div className={styles.progressBar} style={{width: progressPercent}}/>
+
+      {canSeek
+        ? <div className={styles.progressPointer} style={{left: progressPercent}}>
+            <div className={styles.progressPointerGlow} />
+          </div>
+        : null}
     </div>;
   }
 
-  _mouseEnter(state) {
-    if (!this.props.canSeek) {
-      return;
-    }
-
-    this.setState({mouseEnter: state});
-  }
-
-  _seek(e) {
+  _seek = (e) => {
     if (!this.props.canSeek) {
       return;
     }
@@ -56,94 +51,4 @@ export default class HeaderPlayerProgress extends Component {
     const pos = (this.props.max / width) * (e.clientX - nodeOffset(node).left);
     this.props.onSeek(Math.round(pos));
   }
-
-  getStyles() {
-    const {isLoading, canSeek} = this.props;
-    const {mouseEnter} = this.state;
-    const progressPercent = ((100 / this.props.max) * this.props.current) + '%';
-
-    const bufferBackground = {
-      backgroundImage: 'radial-gradient(rgb(169, 192, 233) 0%, rgb(169, 192, 233) 24%, transparent 50%)',
-      backgroundSize: '10px 10px',
-      backgroundPosition: '0px center',
-      backgroundRepeat: 'repeat-x',
-      animation: 'x 3s infinite linear',
-      animationName: progressBuffer,
-    };
-
-    const playingBackground = {
-      background: 'rgba(255, 255, 255, .24)',
-    };
-
-    return {
-      container: {
-        boxSizing: 'border-box',
-        height: 16,
-        position: 'absolute',
-        bottom: -6,
-        left: 0,
-        right: 0,
-        cursor: canSeek ? 'pointer' : null,
-      },
-
-      background: {
-        position: 'absolute',
-        bottom: 6,
-        left: 0,
-        right: 0,
-        height: 4,
-        zIndex: -1,
-        ...(isLoading ? bufferBackground : playingBackground),
-      },
-
-      progress: {
-        position: 'absolute',
-        backgroundColor: colors.accent,
-        height: 4,
-        left: 0,
-        bottom: 6,
-        width: progressPercent,
-      },
-
-      pointer: {
-        zIndex: 10,
-        position: 'absolute',
-        top: 2,
-        left: progressPercent,
-        marginLeft: -8,
-        width: 12,
-        height: 12,
-        borderRadius: '50%',
-        backgroundColor: colors.accent,
-        opacity: mouseEnter ? 1 : 0,
-        transform: 'scale(' + (mouseEnter ? '1' : '.3') +')',
-        transition: 'opacity .1s ease-out, transform .2s ease-out',
-      },
-
-      pointerGlow: {
-        position: 'absolute',
-        top: -6,
-        left: -6,
-        width: 24,
-        height: 24,
-        borderRadius: '50%',
-        backgroundColor: colors.accent,
-        opacity: .24,
-      },
-    };
-  }
 }
-
-const progressBuffer = Radium.keyframes({
-  '0%': {
-    opacity: 1,
-    backgroundPosition: '0px center',
-  },
-  '50%': {
-    opacity: 0,
-  },
-  '100%': {
-    opacity: 1,
-    backgroundPosition: '-200px center',
-  },
-});

d6a77109939b32e401cf2fd8bbec1c18cfe1e507
grid <gridsane@gmail.com>
2016-05-23 10:48:47 +0500

    playlit track component, playlist to css-modules

---
+++ test/components/feed/feed-playlist-track_test.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import Track from '../../../src/components/feed/feed-playlist-track';
+import {Avatar, Icon} from '../../../src/components/ui';
+import {shallowRender, renderDOM} from '../../utils';
+import {Simulate} from 'react-addons-test-utils';
+import {findAllWithClass} from 'react-shallow-testutils';
+import styles from '../../../src/components/feed/feed.scss';
+
+describe('Playlist track component', () => {
+  const defaultProps = {
+    id: '1',
+    artist: 'Foo',
+    title: 'Bar',
+    channelImage: 'foo_image.jpg',
+    duration: 65,
+    onClick: () => null,
+  };
+
+  it('renders title, artist and duration', () => {
+    const dom = renderDOM(<Track  {...defaultProps} />);
+
+    expect(dom.textContent).toContain('Bar by Foo01:05');
+  });
+
+  it('renders channel image', () => {
+    const track = shallowRender(<Track {...defaultProps} />);
+    const channelImage = track.props.rightElement;
+
+    expect(channelImage.type).toBe(Avatar);
+    expect(channelImage.props.url).toBe('foo_image.jpg');
+    expect(channelImage.props.size).toBe(32);
+    expect(channelImage.props.className).toBe(styles.trackChannelImage);
+
+  });
+
+  it('renders idle state', () => {
+    const track = shallowRender(<Track {...defaultProps} />);
+
+    expect(track.props.leftElement).toBe(null);
+    expect(track.props.className).toBe(styles.track);
+  });
+
+  it('renders selected state', () => {
+    const track = shallowRender(<Track {...defaultProps} isCurrent={true} />);
+
+    expect(track.props.className).toContain(styles.trackCurrent);
+    expect(track.props.leftElement.type).toBe(Icon);
+    expect(track.props.leftElement.props.glyph).toBe('play_arrow');
+    expect(track.props.leftElement.props.className).toBe(styles.trackCurrentIcon);
+  });
+
+  it('renders error state', () => {
+    const track = shallowRender(<Track {...defaultProps} error={'some error'} />);
+
+    expect(track.props.className).toContain(styles.trackError);
+    expect(track.props.leftElement.type).toBe(Icon);
+    expect(track.props.leftElement.props.glyph).toBe('error');
+  });
+
+  it('responds with id to onClick callback', () => {
+    const spy = expect.createSpy();
+    const props = {...defaultProps, onClick: spy};
+    const dom = renderDOM(<Track {...props} />);
+
+    Simulate.click(dom);
+
+    expect(spy.calls.length).toBe(1);
+    expect(spy.calls[0].arguments[0]).toBe('1');
+  });
+
+  it('does not render channel image if image is empty', () => {
+    const track = shallowRender(<Track {...defaultProps} channelImage={null} />);
+
+    expect(track.props.rightElement).toBe(null);
+  });
+
+  it('does not render artist if artist is empty', () => {
+    const track = shallowRender(<Track {...defaultProps} artist={null} />);
+
+    expect(
+      findAllWithClass(track.props.primaryText, styles.trackArtist).length
+    ).toBe(0);
+
+  });
+});
--- test/components/feed/feed-playlist_test.js
+++ test/components/feed/feed-playlist_test.js
@@ -1,75 +1,64 @@
 import React from 'react';
 import Playlist from '../../../src/components/feed/feed-playlist';
+import Track from '../../../src/components/feed/feed-playlist-track';
 import {LazyList, ListLabel} from '../../../src/components/ui';
 import {findWithType} from 'react-shallow-testutils';
 import {shallowRender, renderDOM} from '../../utils';
+import {Simulate} from 'react-addons-test-utils';
 
 describe('Playlist component', () => {
-
-  const tracks = [
-    {id: '1', title: 'foo_title', artist: 'foo_artist', duration: 65},
-    {id: '2', title: 'bar_title', artist: 'bar_artist', duration: 125},
-    {id: '3', title: 'baz_title', artist: null, duration: 185},
-  ];
-
-  var result, lazyList, renderItem, renderItemDOM, selectSpy, toggleShuffleSpy;
-
-  beforeEach(() => {
-    selectSpy = expect.createSpy();
-    toggleShuffleSpy = expect.createSpy();
-
-    result = shallowRender(
-      <Playlist
-        tracks={tracks}
-        currentTrackId={'2'}
-        onSelect={selectSpy}
-        onToggleShuffle={toggleShuffleSpy} />
-    );
-    lazyList = findWithType(result, LazyList);
-    renderItem = lazyList.props.renderItem;
-    renderItemDOM = (...args) => renderDOM(lazyList.props.renderItem(...args));
-  });
+  const defaultProps = {
+    tracks: [
+      {id: '1', title: 'foo_title', artist: 'foo_artist', duration: 65},
+      {id: '2', title: 'bar_title', artist: 'bar_artist', duration: 125},
+      {id: '3', title: 'baz_title', artist: null, duration: 185},
+    ],
+    currentTrackId: '2',
+    onSelect: () => null,
+    onToggleShuffle: () => null,
+  };
 
   it('renders tracks', () => {
-    expect(lazyList.props.items.length).toBe(3);
-    expect(renderItemDOM(tracks[0]).textContent).toBe('foo_title by foo_artist01:05');
-    expect(renderItemDOM(tracks[2]).textContent).toBe('baz_title03:05');
-  });
+    const playlist = shallowRender(<Playlist {...defaultProps} />);
+    const list = findWithType(playlist, LazyList);
+    const track = list.props.renderItem(defaultProps.tracks[0]);
 
-  it('marks current track', () => {
-    expect(renderItem(tracks[0]).props.leftElement).toBe(null);
-    expect(renderItem(tracks[1]).props.leftElement).toNotBe(null);
+    expect(list.props.items).toEqual(defaultProps.tracks);
+    expect(track.type).toBe(Track);
+    expect(track.key).toBe('1');
+
+    const currentTrack = list.props.renderItem(defaultProps.tracks[1]);
+    expect(currentTrack.props.isCurrent).toBe(true);
   });
 
   it('selects track', () => {
-    renderItem({id: '99'}).props.onClick();
-    expect(selectSpy.calls[0].arguments).toEqual(['99']);
-  });
+    const spy = expect.createSpy();
+    const playlist = shallowRender(<Playlist {...defaultProps} onSelect={spy} />);
+    const list = findWithType(playlist, LazyList);
+    const track = list.props.renderItem(defaultProps.tracks[2]);
 
-  it('shows error on track', () => {
-    expect(renderItemDOM(tracks[0]).textContent).toNotContain('error');
-    expect(renderItemDOM({...tracks[0], error: null}).textContent).toContain('error');
-    expect(renderItemDOM({...tracks[0], error: 'message'}).textContent).toContain('error');
+    Simulate.click(renderDOM(track));
+
+    expect(spy.calls.length).toBe(1);
+    expect(spy.calls[0].arguments[0]).toBe('3');
   });
 
   it('calls onToggleShuffle callback', () => {
-    const label = findWithType(result, ListLabel);
+    const spy = expect.createSpy();
+    const playlist = shallowRender(<Playlist {...defaultProps} onToggleShuffle={spy} />);
+    const label = findWithType(playlist, ListLabel);
+
     label.props.rightElement.props.onClick();
 
-    expect(toggleShuffleSpy.calls[0].arguments).toEqual([]);
+    expect(spy.calls.length).toBe(1);
+    expect(spy.calls[0].arguments).toEqual([]);
   });
 
   it('shows empty state', () => {
-    const emptyResult = renderDOM(
-      <Playlist
-        tracks={[]}
-        currentTrackId={null}
-        onSelect={selectSpy}
-        onToggleShuffle={toggleShuffleSpy} />
-    );
+    const dom = renderDOM(<Playlist {...defaultProps} tracks={[]} />);
 
-    expect(emptyResult.textContent).toNotContain('shuffle');
-    expect(emptyResult.textContent).toNotContain('0');
-    expect(emptyResult.textContent).toContain('empty');
+    expect(dom.textContent).toNotContain('shuffle');
+    expect(dom.textContent).toNotContain('0');
+    expect(dom.textContent).toContain('empty');
   });
 });

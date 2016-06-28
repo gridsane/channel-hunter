ea0e1cc6cb055b630fb1857646b5cc55785ce313
grid <gridsane@gmail.com>
2016-06-28 23:56:49 +0500

    tags parser

---
+++ test/utils/parse-tags_test.js
@@ -0,0 +1,22 @@
+import parseTags from '../../src/utils/parse-tags';
+
+describe('Tag parser', () => {
+
+  it('parses tags from text', () => {
+    expect(parseTags('this #string contain #awesome tags')).toEqual([
+      'string',
+      'awesome',
+    ]);
+
+    expect(parseTags('#tag1 #tag2 #tag3')).toEqual(['tag1', 'tag2', 'tag3']);
+    expect(parseTags('hello #world #world')).toEqual(['world']);
+    expect(parseTags('no tags')).toEqual([]);
+  });
+
+  it('normalizes tags', () => {
+    expect(parseTags('#Stoner #stoner #stOner')).toEqual(['Stoner']);
+    expect(parseTags('#Stoner_rock #stonerRock')).toEqual(['StonerRock']);
+    expect(parseTags('#tag_2007')).toEqual(['tag2007']);
+  });
+
+});

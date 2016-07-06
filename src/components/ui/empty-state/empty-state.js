a4dc57e0b7bc67a01106064d3db4d89a70c03c62
grid <gridsane@gmail.com>
2016-07-06 15:15:27 +0500

    no channels state with go to discover button

--- src/components/ui/empty-state/empty-state.js
+++ src/components/ui/empty-state/empty-state.js
@@ -4,8 +4,10 @@ import cn from 'classnames';
 import styles from './empty-state.scss';
 
 export default function EmptyState(props) {
-  return <div className={cn(styles.root, props.className)}>
-    <Icon className={styles.icon} size={72} glyph={props.glyph} />
+  return <div className={cn(styles.root, props.className, {
+    [styles.rootSmall]: props.small,
+  })}>
+    <Icon className={styles.icon} size={props.small ? 36 : 72} glyph={props.glyph} />
     <h2 className={styles.primaryText}>{props.primaryText}</h2>
     {props.secondaryText
       ? <span className={styles.secondaryText}>{props.secondaryText}</span>
